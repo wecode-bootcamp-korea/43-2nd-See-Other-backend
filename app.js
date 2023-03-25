e434eda6d77b10cd6bc84e4f4d1a3a66b6a1ea53
HyukJu <HyukJu@HyukJu.local>
2023-03-25 18:00:59 +0900

    [ADD] 초기세팅

---
+++ app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const dbDataSource = require("./api/models/dataSource");
+
+dbDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => {
+    console.error("Error during Data Source initialization", error);
+  });
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(morgan("combined"));
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+  res.status(200).json({ message: "pong" });
+});
+
+app.all("*", (req, res, next) => {
+  const err = new Error(`Can't fine ${req.originalUrl} on this server!`);
+
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.listen(PORT, "127.0.0.1", () => {
+  console.log(`🚀🚀🚀 Server Listening to request on 127.0.0.1:${PORT} 🚀🚀🚀`);
+});
