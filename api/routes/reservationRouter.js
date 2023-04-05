8a2f4144ff283d8dabe19a72729708c6a50960e1
Dohangang <121237964+Dohangang@users.noreply.github.com>
2023-04-05 23:08:46 +0900

    [ADD] 예매 시간 옵션 read

--- api/routes/reservationRouter.js
+++ api/routes/reservationRouter.js
@@ -4,5 +4,6 @@ const reservationController = require("../controllers/reservationController");
 const router = express.Router();
 
 router.get("", reservationController.getOptions);
+router.get("/times", reservationController.getTimes);
 
 module.exports = router;
--- api/services/reservationService.js
+++ api/services/reservationService.js
@@ -1,5 +1,9 @@
 const reservationDao = require("../models/reservationDao");
-const { movieOptionBuilder } = require("../models/builder");
+const {
+  movieOptionBuilder,
+  upperWhereClauseBuilder,
+  lowerWhereClauseBuilder,
+} = require("../models/builder");
 
 const getOptions = async (movie, cinemaName, hallType, date) => {
   const whereClause = await movieOptionBuilder(
@@ -10,13 +14,10 @@ const getOptions = async (movie, cinemaName, hallType, date) => {
   );
 
   const listMovieOptions = await reservationDao.listMovieOptions(whereClause);
-
   const listRegionOptions = await reservationDao.listRegionOptions(whereClause);
-
   const listHallTypeOptions = await reservationDao.listHallTypeOptions(
     whereClause
   );
-
   const listDateOptions = await reservationDao.listDateOptions(whereClause);
 
   return {
@@ -27,6 +28,28 @@ const getOptions = async (movie, cinemaName, hallType, date) => {
   };
 };
 
+const getTimes = async (movie, cinemaName, hallType, date) => {
+  const upperWhereClause = await upperWhereClauseBuilder(
+    movie,
+    cinemaName,
+    hallType,
+    date
+  );
+  const lowerWhereClause = await lowerWhereClauseBuilder(
+    movie,
+    cinemaName,
+    hallType,
+    date
+  );
+
+  const getTimes = await reservationDao.listTimes(
+    upperWhereClause,
+    lowerWhereClause
+  );
+  return getTimes;
+};
+
 module.exports = {
   getOptions,
+  getTimes,
 };
